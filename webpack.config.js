cbe87a005fcbd793e191914d5aa2323ba32928f3
Задорин Артем <artfan95@gmail.com>
2024-02-03 15:00:52 +0700

    Сборка готовой html разметки из предыдущих блоков

--- webpack.config.js
+++ webpack.config.js
@@ -67,7 +67,7 @@ module.exports = {
   plugins: [
     // Подключаем файл html, стили и скрипты встроятся автоматически
     new HtmlWebpackPlugin({
-      title: 'Webpack 4 Starter',
+      title: 'Kata 6.1.5',
       template: './src/index.html',
       inject: true,
       minify: {
